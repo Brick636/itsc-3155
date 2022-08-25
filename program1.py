10be51af5e322681da5b941d902d81e02f706290
Elijah <ejrogersemail@gmail.com>
2022-08-25 11:38:28 -0400

    Python program 1

--- program1.py
+++ program1.py
@@ -1,12 +1,13 @@
+# function returns first and last two characters of a user string
 def letters():
     word = input("Please enter a string: ")
     print("The string is " + word)
-
-
+# if the user string is less than a length of 2 print user error message
     if len(word) < 2:
-        print("String is not long enough!")
+        print("String is not long enough!")  
+# else return the first and last two characters of the user string            
     else:
         newWord = word[0] + word[1] + word[len(word) - 2] + word[len(word) - 1]
-        print(newWord)
+        return(newWord)
 
-letters()           
+print(letters())           
